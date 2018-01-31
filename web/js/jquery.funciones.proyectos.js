b9eb494b0357da35bfddadfc74a5ee25a566c0a1
sesparza <sair.esparza@patronato.unam.mx>
2018-01-31 11:52:34 -0600

    actualizacion

--- web/js/jquery.funciones.proyectos.js
+++ web/js/jquery.funciones.proyectos.js
@@ -186,8 +186,8 @@ function  validar_etapas()
 		$("#importe_asignado").parent().children("span").text("").hide();
 	 
 	}
-        
-        var valor = document.getElementById("importe_asignado2").value;
+         $('#TabProy a[href="#resp"]').tab('show');
+        /*var valor = document.getElementById("importe_asignado2").value;
 	if ( valor == null || valor.length==0  ||  !(/\d{1,10}\.\d{2}/.test(valor)))
 	{
 	  $("#iconotexto").remove();
@@ -201,7 +201,7 @@ function  validar_etapas()
 		$("#importe_asignado2").parent().attr("class","form-group  has-success has-feedback" );
 		$("#importe_asignado2").parent().children("span").text("").hide();
                 $('#TabProy a[href="#resp"]').tab('show');
-	}
+	}*/
        
 }
 
@@ -506,4 +506,4 @@ function eliminar_etapa(cualtel){
 			break;
 	}
 	
-}  
+}       
